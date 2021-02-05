dd5f1c52c9554074df52b8c65df9f8978ff78dea
Bohdan-Khasanov <bohdan.khasanov@gmail.com>
2021-02-05 16:21:47 +0200

    added OIDC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+
+  constructor(
+    public oidcSecurityService: OidcSecurityService,
+    public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.oidcSecurityService
+      .checkAuth()
+      .subscribe((auth) => console.log('is authenticated', auth));
+  }
+
+  login(): void {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout(): void {
+    let isAuth: boolean;
+    this.oidcSecurityService.checkAuth().subscribe(value => {
+      isAuth = value;
+    });
+    if (isAuth) {
+      this.oidcSecurityService.logoff();
+    }
+  }
+
+
+  callApi(): void {
+    const token = this.oidcSecurityService.getToken();
+
+    this.http.get('http://localhost:5000/Organization/TestOk', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+      responseType: 'text'
+    })
+      .subscribe((data: any) => {
+        console.log('api result:', data);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuthModule, LogLevel, OidcConfigService} from 'angular-auth-oidc-client';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+
+export function configureAuth(oidcConfigService: OidcConfigService): any {
+  return () =>
+    oidcConfigService.withConfig({
+      clientId: 'angular',
+      stsServer: 'http://localhost:5443',
+      responseType: 'code',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      scope: 'openid outofschoolapi.read',
+      logLevel: LogLevel.Debug,
+    });
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
